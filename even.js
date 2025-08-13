bc555c70a534312e23b6a0640652ae3559bf7e03
SURIYAPRASATH S <suriyaprasath161@gmail.com>
2025-08-13 12:50:25 +0530

    Add files via upload

---
+++ even.js
@@ -0,0 +1,44 @@
+var card=document.getElementById('card');
+var message=document.getElementById('message');
+card.addEventListener('mouseover',function(){
+       card.style.backgroundColor="green";
+       message.innerHTML="You Are IN";
+});
+card.addEventListener('mouseout',function(){
+      card.style.backgroundColor="red";
+      message.innerHTML="You Are Out";
+});
+card.addEventListener('click',function(){
+      card.style.backgroundColor="yellow";
+      message.innerHTML="You Clicked ";
+});
+
+
+function updateClock(){
+    var date=new Date();
+    var hours=date.getHours().toString().padStart(2,'0');
+    var mins=date.getMinutes().toString().padStart(2,'0');
+    var secs=date.getSeconds().toString().padStart(2,'0');
+    document.getElementById('clock').innerHTML=hours+":"+mins+":"+secs;
+    playSound();
+
+}
+let clickSound = new Audio("WhatsApp Audio 2025-08-13 at 12.43.51_937cdd0c.mp3");
+clickSound.autoplay = true; 
+clickSound.muted = false;   
+
+function playSound(){
+    clickSound.currentTime=0;
+    clickSound.play().catch(err => {
+        console.warn("Autoplay blocked by browser:", err);
+    });
+}
+
+window.setInterval(updateClock,1000);
+updateClock();
+
+
+
+
+
+
